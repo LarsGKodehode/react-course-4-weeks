7a0db291b2f5e9ca259fe930520a54441ff42117
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-11-03 11:54:16 +0100

    fixed StylingGlobal

--- src/components/StylingGlobal/StylingGlobal.jsx
+++ src/components/StylingGlobal/StylingGlobal.jsx
@@ -1,8 +1,10 @@
 const StylingGlobal = () => {
   return (
-    <div>
-      <h1>Gloabl CSS</h1>
-      <p>This component</p>
+    <div className="example-div">
+      <h1 className="example-h1">Gloabl CSS</h1>
+      <p className="example-p">This component utilizes global accessible CSS classes.</p>
     </div>
   );
 };
+
+export default StylingGlobal;
--- src/components/StylingGlobal/index.js
+++ src/components/StylingGlobal/index.js
@@ -1,3 +1,3 @@
 // This file is optional, but structuring our component in this way allows us to
 // simplifies our import statements
-export { default as InfoCard } from './InfoCard';
+export { default as StylingGlobal } from './StylingGlobal';
